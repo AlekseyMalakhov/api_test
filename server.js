6bd0781694e1272df5fa0b0ea527da989fbd78fb
Aleksey Malakhov <aleksey.malakhov.eng@gmail.com>
2024-05-07 11:37:58 +0300

    some more routes

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 import express from "express";
+import fs from "node:fs";
+
+let data = null;
+
+try {
+    data = fs.readFileSync("./family.json", "utf8");
+} catch (err) {
+    console.error(err);
+}
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.get("/family", (req, res) => {
+    res.send(data);
+});
+
+app.post("/family", (req, res) => {
+    const body = req.body;
+    console.log(body);
+    res.send("hi all");
 });
 
 app.listen(port, () => {
